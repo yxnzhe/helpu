66def8ff49fd9a17b37affd887482f0368fad642
Leow Yii Cherng <yiicherngleow@gmail.com>
2021-07-10 22:23:33 +0800

    login function

--- function.php
+++ function.php
@@ -1,5 +1,5 @@
 <?php
-    function connectDb(){ //Connect to database
+    function connectDb() { //Connect to database
         $servername = "localhost";
         $username = "root";
         $password = "";
@@ -7,20 +7,20 @@
 
         $conn = mysqli_connect($servername, $username, $password, $db);
 
-        if(!$conn){
+        if(!$conn) {
             die ("Connection failed: ". mysqli_connect_error()); //Return error message if there's error when connecting to the database
         }
         return $conn;
     }
 
     function register($name, $email, $password){ //Register function
-        if($name == "" || $email == "" || $password == ""){ //Check whether all field is filled up with value
+        if($name == "" || $email == "" || $password == "") { //Check whether all field is filled up with value
             echo "All field is Mandatory!";
         }
         else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //Check whether the email format is correct or not
             echo "Invalid email format!";
         }
-        else{ //If all field is with value and the email format is correct, then will create the user in our database
+        else { //If all field is with value and the email format is correct, then will create the user in our database
             $conn = connectDb();
             $password = password_hash($password, PASSWORD_DEFAULT);
 
@@ -35,5 +35,41 @@
                 echo "Error: ".$sql."<br>".mysqli_error($conn);
             }
         }
+    }
+
+    function login($email, $password) {
+        if($email == "" || $password == "") {
+            echo "All field is Mandatory!";
+        }
+        else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "Invalid email format!";
+        }
+        else {
+            $conn = connectDb();
+            $sql = "SELECT `id`, password FROM user WHERE email = ?";
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $email);
+
+            if($stmt->execute()) {
+                $stmt->bind_result($id, $pass);
+                if($stmt->fetch()) {
+                    if(password_verify($password, $pass)) {
+                        $_SESSION["userId"] = $id;
+                    }
+                    else {
+                        echo "Login Failed. Invalid Username/Password.";
+                    }
+                }
+                else {
+                    echo "Login Failed. Invalid Username/Password.";
+                }
+            }
+            else {
+                echo $conn->error;
+            }
+        }
+        $stmt->close();
+        $conn->close();
     }
 ?>

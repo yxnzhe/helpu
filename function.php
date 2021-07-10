de455d9935abdab95c162d2d25d548197e913806
Leow Yii Cherng <yiicherngleow@gmail.com>
2021-07-10 22:36:24 +0800

    added comments

--- function.php
+++ function.php
@@ -40,36 +40,36 @@
         }
     }
 
-    function login($email, $password) {
-        if($email == "" || $password == "") {
-            echo "All field is Mandatory!";
+    function login($email, $password) { //function to allow user to login to the website
+        if($email == "" || $password == "") { //to validate whether email or password is empty
+            echo "All field is Mandatory!"; //if email or password is empty, error message will prompt
         }
-        else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo "Invalid email format!";
+        else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //to validate whether user has entered a valid email format
+            echo "Invalid email format!"; //if email format is invalid, this error message will prompt
         }
-        else {
-            $conn = connectDb();
-            $sql = "SELECT `id`, password FROM user WHERE email = ?";
+        else { //if email and password is entered as required
+            $conn = connectDb(); //connect to databsae
+            $sql = "SELECT `id`, password FROM user WHERE email = ?"; //query to select id and password from user table based on email
 
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("s", $email);
+            $stmt = $conn->prepare($sql); //to prepare the query
+            $stmt->bind_param("s", $email); //to bind the email with the email entered by user
 
-            if($stmt->execute()) {
-                $stmt->bind_result($id, $pass);
-                if($stmt->fetch()) {
-                    if(password_verify($password, $pass)) {
-                        $_SESSION["userId"] = $id;
+            if($stmt->execute()) { //to execute
+                $stmt->bind_result($id, $pass); //to bind the result of id and password based on the query
+                if($stmt->fetch()) { //to fetch
+                    if(password_verify($password, $pass)) { //to verify whether the password entered by user is equal to the password in the database
+                        $_SESSION["userId"] = $id; //if password is entered correctly, $_SESSION["userId] will set to the user's id
                     }
-                    else {
-                        echo "Login Failed. Invalid Username/Password.";
+                    else { //if password is invalid
+                        echo "Login Failed. Invalid Username/Password."; //error message will prompt
                     }
                 }
-                else {
-                    echo "Login Failed. Invalid Username/Password.";
+                else { //if cant fetch
+                    echo "Login Failed. Invalid Username/Password."; //error message will prompt
                 }
             }
-            else {
-                echo $conn->error;
+            else { //if $stmt cant execute
+                echo $conn->error; //error message will prompt
             }
             $stmt->close();
             $conn->close();

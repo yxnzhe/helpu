912e3ea2f8d337cb978087fd320eaa520cc4681c
yxnzhe <71542921+yxnzhe@users.noreply.github.com>
2021-07-11 13:41:12 +0800

    make footer bottom aligned

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <title>Helpu Student Website</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="..\helpu\css\style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="icon" type="image/png" href="imgs/helpu.png" />
 </head>
@@ -42,10 +43,6 @@
     ?>
     <br>
 
-    <?php
-    include 'footer.php';
-    ?>
-
 </body>
-
+<?php include 'footer.php';?>
 </html>

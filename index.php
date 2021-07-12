c1505ae339a79fe5a2dd811b3d2a72619d63af56
Leow Yii Cherng <yiicherngleow@gmail.com>
2021-07-13 00:57:10 +0800

    ..

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 
 <body style="background-color: #f3f3f3">
-    <div class="row justify-content-center" style="margin-bottom: 4rem!important;">
+    <div class="row justify-content-center pt-2" style="margin-bottom: 4rem!important;">
         <div class="col-2"> </div>
         <div class="col-8">
             <div class="card">
@@ -26,22 +26,35 @@
             <br />
             <div class="card">
                 <h5 class="card-header">Posts</h5>
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <form method="POST" class="mb-0">
-                        <div class="mb-3 row">
-                            <div class="col-10 col-lg-11 p-0 pr-1 px-md-3">
-                                <textarea class="form-control" name="comment" rows="1" placeholder="Add a comment..." required></textarea>
-                            </div>
-                            <div class="col-2 p-0 col-lg-1">
-                                <a href="post_content.php" class="btn btn-primary">Post</a>
-                            </div>
-                        </div>
+                <?php
+                    $query = "SELECT * FROM product ORDER BY created_at DESC";
+                    $result = $conn->query($query);
+                    if($result->num_rows > 0) {
+                        while($post = $result->fetch_array()) {
+                ?>
+                            <div class="card-body">
+                                <h5 class="card-title">Special title treatment</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <form method="POST" class="mb-0">
+                                    <div class="mb-3 row">
+                                        <div class="col-10 col-lg-11 p-0 pr-1 px-md-3">
+                                            <textarea class="form-control" name="comment" rows="1" placeholder="Add a comment..." required></textarea>
+                                        </div>
+                                        <div class="col-2 p-0 col-lg-1">
+                                            <a href="post_content.php" class="btn btn-primary">Post</a>
+                                        </div>
+                                    </div>
 
-                        <button type="button" class="btn btn-danger">Delete post</button>
-                    </form>
-                </div>
+                                    <button type="button" class="btn btn-danger">Delete post</button>
+                                </form>
+                            </div>
+                <?php
+                        }
+                    }
+                    else {
+
+                    }
+                ?>
             </div>
         </div>
         <div class="col-2"> </div>

f94a490ff2557cf509e3990cf8d166e2304ba730
Michellexinhui0 <Michlxh011001@gmail.com>
2021-07-14 01:36:05 +0800

    new one

--- post_content.php
+++ post_content.php
@@ -1,4 +1,5 @@
 <html>
+
 <head>
     <?php
     require_once "navbar.php";
@@ -6,50 +7,37 @@
 </head>
 
 <body>
-
-<!-- <div class="card">
+    <div class="row justify-content-center pt-2 " style="margin-bottom: 4rem!important;">
+        <div class="col-2"> </div>
+        <div class="col-8">
+            <div class="card ">
                 <h5 class="card-header">Posts</h5>
-                <?php
-                    $getPost = getPost($postId);
+                <div class="row justify-content-center">
 
-                    if(count($getPost) > 0) {
-                        foreach ($getPost as $i) {
-                            print_r(getComment($i['id']));
-                ?>
-                            <div class="card-body mb-1">
-                                <h5 class="card-title"><?php echo $i["name"]; ?></h5>
-                                <p class="card-text"><?php echo $i["content"];?></p>
-                                <form method="POST" class="mb-0">
-                                    <div class="mb-3 row">
-                                        <div class="col-10 col-lg-11 p-0 pr-1 px-md-3">
-                                            <textarea onkeyup="countCharComment(this)" class="form-control" name="comment" rows="1" placeholder="Add a comment..." required></textarea>
-                                            <div class="pt-1 text-right" id="charNumComment"></div>
-                                        </div>
-                                        <div class="col-2 p-0 col-lg-1">
-                                            <input type=submit name="post_button" class="btn btn-primary" value="Post" />
-                                        </div>
-                                        <input type="hidden" value= <?php echo $i["id"]?> name="post_id" />
-                                        <!-- <p><?php echo count(getPost($i["id"])); ?></p> -->
-                                    </div>
-                                </form>
-                                <?php 
-                                    if(isset($_SESSION["userId"])) { 
-                                        if($i["user_id"] == $_SESSION["userId"]) { ?>
-                                            <form method="POST">
-                                                <input type="hidden" value= <?php echo $i["id"]?> name="post_id" />
-                                                <input type="submit" class="btn btn-danger" name="deletePost" value="Delete Post">
-                                            </form>
-                                        <?php 
-                                        } 
-                                    }
-                                ?>
+                    <div class="card-body mb-1 mx-2">
+                        <h5 class="card-title">name</h5>
+                        <p class="card-text">post content</p>
+
+                        <form method="POST" class="mb-0">
+                            <div class="mb-3 row">
+                                <div class="col-10 col-lg-11 p-0 pr-1 px-md-3">
+                                    <textarea class="form-control" name="comment" rows="1" placeholder="Add a comment..." required></textarea>
+                                    <div class="pt-1 text-right" id="charNumComment"></div>
+                                </div>
+                                <div class="col-2 p-0 col-lg-1">
+                                    <input type=submit name="post_button" class="btn btn-primary" value="Post" />
+                                </div>
+
                             </div>
-                            <hr style="border-top: 25px solid #f3f3f3; margin: 0">
-                <?php
-                        }
-                    }
-                   
-                ?>   -->
-    
+                        </form>
+
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-2"> </div>
+    </div>
 </body>
+
 </html>
